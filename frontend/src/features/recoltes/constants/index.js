51d4331e824a7ef88f6094bd8f02e7c2ecedd48f
lepekinoi <59528735+lepekinoi@users.noreply.github.com>
2026-01-23 16:43:22 +0100

    refactor(recoltes): add constants barrel export

---
+++ frontend/src/features/recoltes/constants/index.js
@@ -0,0 +1,4 @@
+// Barrel export de toutes les constantes
+export { EXPOSITIONS } from './expositions';
+export { PAGINATION_OPTIONS } from './pagination';
+export { QUALITES_VENDABLES, QUALITES_NON_VENDABLES } from './qualites';
