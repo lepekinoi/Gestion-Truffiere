bd3fef533da869683f46c0cc2384da01004cda4c
lepekinoi <59528735+lepekinoi@users.noreply.github.com>
2026-01-24 20:43:32 +0100

    feat: add main export file for arbres feature

---
+++ frontend/src/features/arbres/index.js
@@ -0,0 +1,17 @@
+// Export des pages
+export { ArbresPage } from './pages';
+
+// Export des composants
+export * from './components';
+
+// Export des hooks
+export { useArbres } from './hooks';
+
+// Export des services
+export * from './services';
+
+// Export des constantes
+export * from './constants';
+
+// Export des utilitaires
+export * from './utils';
